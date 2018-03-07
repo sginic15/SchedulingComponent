02e3193f6eb5e591771f3505e015c15f81eac1d9
fhadziristic15 <fhadziristic15@192.168.56.1>
2018-03-07 11:16:22 +0100

    my commit

--- ShedulingComponent/src/test.java
+++ ShedulingComponent/src/test.java
@@ -24,13 +24,13 @@ public class test {
 
 		Schedule schedule = new Schedule();
 		EventFactory eventFactory = new EventFactory();
-		Event workEvent1 = eventFactory.getEvent("FUN");
+		Event workEvent1 = eventFactory.getEvent("FunEvent");
 		workEvent1.setStartActivity(LocalDateTime.of(2018, 1, 12, 12, 25));
 		workEvent1.setEndActivity(LocalDateTime.of(2018, 1, 20, 12, 25));
 		workEvent1.setTitle("event11111");
 		schedule.add(workEvent1);
 
-		Event workEvent2 = eventFactory.getEvent("WORK");
+		Event workEvent2 = eventFactory.getEvent("WorkEvent");
 		workEvent2.setStartActivity(LocalDateTime.of(2018, 1, 13, 12, 25));
 		workEvent2.setEndActivity(LocalDateTime.of(2018, 1, 25, 12, 25));
 		workEvent2.setTitle("event22222222");
